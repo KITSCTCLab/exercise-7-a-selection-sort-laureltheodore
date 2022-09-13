72c8dfc875a2637f34fb7a2efef87566cebc276c
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-09-13 11:45:01 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ data = []
 for item in input_data.split(', '):
   if item.isnumeric():
     data.append(int(item))
-   elif item.lstrip("-").isnumeric():
-     data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
 print(selectionSort(data, len(data)))
 
