280f235056db777b48b7d611970794c0478080fa
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-09-13 11:32:59 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 from typing import List
 def selectionSort(array, size) -> List[int]:
-for index in range(size):
-min = index
-Selection and Insertion Sorting
-for j in range(index+1,size):
-if array[j]<array[min]:
-min = j
-(array[min],array[index]) = (array[index],array[min])
-return array
+  for index in range(size):
+    min = index
+    Selection and Insertion Sorting
+    for j in range(index+1,size):
+      if array[j]<array[min]:
+        min = j
+        (array[min],array[index]) = (array[index],array[min])
+        return array
 
 input_data = input()
 data = []
