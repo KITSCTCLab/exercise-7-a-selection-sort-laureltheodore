2d375ba83eb4c45a0b21acb169b69c174ad6c7b3
laureltheodore <110010515+laureltheodore@users.noreply.github.com>
2022-09-13 11:41:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,12 +8,13 @@ def selectionSort(array, size) -> List[int]:
         min = j
         (array[min],array[index]) = (array[index],array[min])
         return array
-      input_data = input()
-      data = []
-      for item in input_data.split(', '):
-        if item.isnumeric():
-          data.append(int(item))
-          elif item.lstrip("-").isnumeric():
-            data.append(int(item))
-            print(selectionSort(data, len(data)))
+#do not change the code
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+    elif item.lstrip("-").isnumeric():
+      data.append(int(item))
+      print(selectionSort(data, len(data)))
 
